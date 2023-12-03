da884ac11733610f31141cd73f877131fe416b81
Nsir Dorra <dorransir24@gmail.com>
2023-12-03 11:18:38 +0100

    up

--- src/app/service/article.service.spec.ts
+++ src/app/service/article.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { LoginService } from './article.service';
+import { ArticleService } from './article.service';
 
 describe('LoginService', () => {
-  let service: LoginService;
+  let service: ArticleService ;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(LoginService);
+    service = TestBed.inject(ArticleService );
   });
 
   it('should be created', () => {
